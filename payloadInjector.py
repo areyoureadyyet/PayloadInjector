43d17bc7c11cda5f6c1566321b2541f8aa3ea030
soumeswar <132776933+soumeswar@users.noreply.github.com>
2023-10-23 19:35:20 +0530

    payloadInjector.py

--- payloadInjector.py
+++ payloadInjector.py
@@ -7,15 +7,14 @@ txt2 = "Created By TheRedHackingHub"
 centered_txt2 = txt2.center(50)
 print(centered_txt2)
 try:
-	exe_file = input("[-] The Main Executable File: ")
-	payload_file = input("[+] The Payload File To Inject: ")
-	exef = open(str(exe_file), "ab")
-	payf = open(str(payload_file), "rb")
-	exef.append("\n")
-	exef.append(payf)
-	print()
-	print("[*] Execution Succesful!")
+        exe_file = input("[-] The Main Executable>
+        payload_file = input("[+] The Payload Fil>
+        exef = open(str(exe_file), "a+")
+        payf = open(str(payload_file), "rb")
+        exef.write("\n")
+        exef.write(str(payf))
+        print()
+        print("[*] Execution Succesful!")
 except KeyboardInterrupt:
-	print()
-	print("[÷] Program Closed")
-
+        print()
+        print("[÷] Program Closed")
