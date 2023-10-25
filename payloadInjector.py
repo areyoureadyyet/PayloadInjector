e9644df7a7f119955977006f1a885a09d6d402c0
soumeswar <132776933+soumeswar@users.noreply.github.com>
2023-10-25 15:36:04 +0530

    payloadInjector.py

--- payloadInjector.py
+++ payloadInjector.py
@@ -7,9 +7,9 @@ txt2 = "Created By TheRedHackingHub"
 centered_txt2 = txt2.center(50)
 print(centered_txt2)
 try:
-        exe_file = input("[-] The Main Executable>
-        payload_file = input("[+] The Payload Fil>
-        exef = open(str(exe_file), "a+")
+        exe_file = input("[-] The Main Executable: ")
+        payload_file = input("[+] The Payload File: ")
+        exef = open(str(exe_file), "ab")
         payf = open(str(payload_file), "rb")
         exef.write("\n")
         exef.write(str(payf))
