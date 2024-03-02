17b5a62545fc4b28cecf10dc1353c14fd525ec3f
CringeGuy69 <132776933+soumeswar@users.noreply.github.com>
2024-03-02 10:39:41 +0530

    Update payloadInjector.py

--- payloadInjector.py
+++ payloadInjector.py
@@ -3,16 +3,15 @@ print(Fore.RED + "")
 txt = "PayloadInjector"
 centered_txt = txt.center(50)
 print(centered_txt)
-txt2 = "Created By TheRedHackingHub"
+txt2 = "Created By SOUMESWAR"
 centered_txt2 = txt2.center(50)
 print(centered_txt2)
 try:
         exe_file = input("[-] The Main Executable: ")
         payload_file = input("[+] The Payload File: ")
-        exef = open(str(exe_file), "ab")
-        payf = open(str(payload_file), "rb")
-        exef.write("\n")
-        exef.write(str(payf))
+        exef = open(exe_file, "ab")
+        payf = open(payload_file, "rb")
+        exef.write(payf)
         print()
         print("[*] Execution Succesful!")
 except KeyboardInterrupt:
